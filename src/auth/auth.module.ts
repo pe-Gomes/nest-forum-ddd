7924de97d6707268eb63841e8280b3ecff1eb11f
Pedro Gomes <pcgbernardes@gmail.com>
2024-09-14 14:57:20 -0400

    feat: add authentication module & controller

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -9,11 +9,19 @@ import { Env } from 'src/env'
     PassportModule,
     JwtModule.registerAsync({
       inject: [ConfigService],
-      useFactory: async (configService: ConfigService<Env, true>) => ({
-        privateKey: configService.get('JWT_PRIVATE_KEY'),
-        publicKey: configService.get('JWT_PUBLIC_KEY'),
-        signOptions: { expiresIn: '1d' },
-      }),
+      global: true,
+      useFactory: async (configService: ConfigService<Env, true>) => {
+        const privateKey = configService.get('JWT_PRIVATE_KEY', { infer: true })
+        const publicKey = configService.get('JWT_PUBLIC_KEY', { infer: true })
+
+        return {
+          signOptions: {
+            algorithm: 'RS256',
+          },
+          privateKey: Buffer.from(privateKey, 'base64'),
+          publicKey: Buffer.from(publicKey, 'base64'),
+        }
+      },
     }),
   ],
 })
