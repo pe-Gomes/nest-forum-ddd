de2061d14c84653bca1b8b8eda2a793816ac89aa
Pedro Gomes <pcgbernardes@gmail.com>
2024-09-14 21:43:47 -0400

    feat:add questions repo to db module

--- src/infra/db/database.module.ts
+++ src/infra/db/database.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common'
 import { PrismaService } from './prisma/prisma.service'
+import { QuestionsRepository } from '@/domain/forum/app/repositories/questions-repository'
+import { PrismaQuestionsRepository } from './prisma/repositories/questions-repository'
 
 @Module({
-  providers: [PrismaService],
-  exports: [PrismaService],
+  providers: [
+    PrismaService,
+    {
+      provide: QuestionsRepository,
+      useClass: PrismaQuestionsRepository,
+    },
+  ],
+  exports: [PrismaService, QuestionsRepository],
 })
 export class DatabaseModule {}
