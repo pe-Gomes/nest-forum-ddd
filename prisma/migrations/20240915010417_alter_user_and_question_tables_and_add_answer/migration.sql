425294a87db771d630b73585b23cdcec6513cd84
Pedro Gomes <pcgbernardes@gmail.com>
2024-09-14 21:21:35 -0400

    feat:add new table & update tables

---
+++ prisma/migrations/20240915010417_alter_user_and_question_tables_and_add_answer/migration.sql
@@ -0,0 +1,61 @@
+/*
+  Warnings:
+
+  - You are about to drop the `quesitons` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- CreateEnum
+CREATE TYPE "UserRole" AS ENUM ('INSTRUCTOR', 'STUDENT');
+
+-- DropForeignKey
+ALTER TABLE "quesitons" DROP CONSTRAINT "quesitons_author_id_fkey";
+
+-- AlterTable
+ALTER TABLE "users" ADD COLUMN     "role" "UserRole" NOT NULL DEFAULT 'STUDENT';
+
+-- DropTable
+DROP TABLE "quesitons";
+
+-- CreateTable
+CREATE TABLE "questions" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3),
+    "author_id" TEXT NOT NULL,
+    "best_answer_id" TEXT,
+
+    CONSTRAINT "questions_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "answers" (
+    "id" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3),
+    "author_id" TEXT NOT NULL,
+    "question_id" TEXT NOT NULL,
+
+    CONSTRAINT "answers_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "questions_slug_key" ON "questions"("slug");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "questions_best_answer_id_key" ON "questions"("best_answer_id");
+
+-- AddForeignKey
+ALTER TABLE "questions" ADD CONSTRAINT "questions_author_id_fkey" FOREIGN KEY ("author_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "questions" ADD CONSTRAINT "questions_best_answer_id_fkey" FOREIGN KEY ("best_answer_id") REFERENCES "answers"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "answers" ADD CONSTRAINT "answers_author_id_fkey" FOREIGN KEY ("author_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "answers" ADD CONSTRAINT "answers_question_id_fkey" FOREIGN KEY ("question_id") REFERENCES "questions"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
