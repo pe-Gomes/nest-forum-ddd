44e9f2195685290dbc830fdfb9d9e61b616108bd
Pedro Gomes <pcgbernardes@gmail.com>
2024-09-15 11:08:03 -0400

    fix:student repo create function to return void

--- src/domain/forum/app/repositories/students-repository.ts
+++ src/domain/forum/app/repositories/students-repository.ts
@@ -3,5 +3,5 @@ import { type Student } from '../../enterprise/entities/student'
 export abstract class StudentsRepository {
   abstract getByEmail(email: string): Promise<Student | null>
   abstract getById(id: string): Promise<Student | null>
-  abstract create(student: Student): Promise<Student>
+  abstract create(student: Student): Promise<void>
 }
