9d2a8f599aeea565cfba6dc500b09a2cc86a9561
Pedro Gomes <pcgbernardes@gmail.com>
2024-09-17 17:41:47 -0400

    feat:add notifications table migration

---
+++ prisma/migrations/20240917214128_create_notifications_table/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "notifications" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "read_at" TIMESTAMP(3),
+    "user_id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "notifications_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "notifications" ADD CONSTRAINT "notifications_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
