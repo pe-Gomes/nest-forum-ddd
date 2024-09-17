654e0b4f1e0ead89fc9e05a2661bf86e73d49e1d
Pedro Gomes <pcgbernardes@gmail.com>
2024-09-17 10:45:20 -0400

    feat:update answer & question attachment repos contracts

--- src/domain/forum/app/repositories/answer-attachments-repository.ts
+++ src/domain/forum/app/repositories/answer-attachments-repository.ts
@@ -1,6 +1,8 @@
 import { type AnswerAttachment } from '../../enterprise/entities/answer-attachment'
 
 export abstract class AnswerAttachmentsRepository {
+  abstract createMany(answerAttachments: AnswerAttachment[]): Promise<void>
   abstract getManyByAnswerId(answerId: string): Promise<AnswerAttachment[]>
+  abstract deleteMany(answerAttachments: AnswerAttachment[]): Promise<void>
   abstract deleteManyByAnswerId(answerId: string): Promise<void>
 }
--- src/domain/forum/app/repositories/question-attachments-repository.ts
+++ src/domain/forum/app/repositories/question-attachments-repository.ts
@@ -1,8 +1,10 @@
 import { type QuestionAttachment } from '../../enterprise/entities/question-attachment'
 
 export abstract class QuestionAttachmentsRepository {
+  abstract createMany(attachments: QuestionAttachment[]): Promise<void>
   abstract getManyByQuestionId(
     questionId: string,
   ): Promise<QuestionAttachment[]>
   abstract deleteManyByQuestionId(questionId: string): Promise<void>
+  abstract deleteMany(attachments: QuestionAttachment[]): Promise<void>
 }
