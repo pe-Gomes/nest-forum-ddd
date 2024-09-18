c05005acc00a2e8464ce866b89f6c555a5bca836
Pedro Gomes <pcgbernardes@gmail.com>
2024-09-17 22:22:12 -0400

    feat:update events module

--- src/infra/events/events.module.ts
+++ src/infra/events/events.module.ts
@@ -1,10 +1,15 @@
 import { OnAnswerCreated } from '@/domain/notification/app/subscribers/on-answer-created'
+import { OnSetQuestionBestAnswer } from '@/domain/notification/app/subscribers/on-set-question-best-answer'
 import { SendNotificationUseCase } from '@/domain/notification/app/use-cases/send-notification'
 import { Module } from '@nestjs/common'
 import { DatabaseModule } from '../db/database.module'
 
 @Module({
   imports: [DatabaseModule],
-  providers: [OnAnswerCreated, SendNotificationUseCase],
+  providers: [
+    OnAnswerCreated,
+    OnSetQuestionBestAnswer,
+    SendNotificationUseCase,
+  ],
 })
 export class EventsModule {}
